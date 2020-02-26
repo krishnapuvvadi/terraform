0326b847c907ba75736d784dcdb2ef3c8642ad91
root <chaitanyap1988@gmail.com>
2020-02-26 12:40:19 +0530

    updated file

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 resource "aws_instance" "test-server" {
-<<<<<<< HEAD
-ami = ""
-instance_type = "t2.micro"
-=======
 ami = "ami-0080e4c5bc078760e"
 instance_type = "t2.micro" 
 Count         = "2" 
->>>>>>> master
 tags = {
  Name = "Test Server"
  Environment = "Dev"
